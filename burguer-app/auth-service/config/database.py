e63c9b5481ba11fd3d785f3819417b9b9cb9fa03
costacarv <matheuscostamcc@hotmail.com>
2026-07-18 23:18:15 -0300

    Commit: para teste CI/CD

---
+++ burguer-app/auth-service/config/database.py
@@ -0,0 +1,25 @@
+"""
+Conecta ao  banco de dados MongoDB utilizando as variáveis de ambiente
+"""
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+# Carrega as variáveis de ambiente do arquivo .env
+
+load_dotenv()
+
+#Cria a conexão com o banco de dados MongoDB
+
+client = MongoClient(os.getenv("MONGO_URI"))
+
+# Seleciona o banco de dados
+db = client["burguer_app_db"]
+
+"""função para retornar a instância do banco de dados"""
+def get_db():
+    """Retorna a instância do banco de dados MongoDB.
+    Returns:
+        db: Instância do banco de dados MongoDB.
+    """
+    return db
---
+++ burguer-app/auth-service/controllers/auth_controller.py
@@ -0,0 +1,42 @@
+"""Auth Controller for handling user authentication in the Burguer App."""
+import os
+import sys
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from services.auth_service import login_user
+from models.user_model import serialize_user
+
+auth_bp = Blueprint("auth", __name__)
+
+@auth_bp.route("/login", methods=["GET"])
+def login_page():
+    return render_template("login.html")
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    data = request.form
+    user = login_user(data["email"], data["password"])
+    if not user:
+        flash("Credenciais inválidas")
+        return redirect(url_for("auth.login_page"))
+
+    session["user"] = serialize_user(user)
+    return redirect(url_for("auth.dashboard"))
+
+@auth_bp.route("/register", methods=["GET"])
+def register_page():
+    # Redirect to user-service for user creation
+    return redirect("http://localhost:5001/user/create")
+
+@auth_bp.route("/dashboard")
+def dashboard():
+    user = session.get("user")
+    if not user:
+        return redirect(url_for("auth.login_page"))
+    return render_template("dashboard.html", user=user)
+
+@auth_bp.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("auth.login_page"))
